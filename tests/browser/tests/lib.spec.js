c659867e01755600f48b1b4260be9a72ec372c80
Igor Savin <iselwin@gmail.com>
2026-03-20 00:14:43 +0200

    Cleanup

--- tests/browser/tests/lib.spec.js
+++ tests/browser/tests/lib.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 async function waitForMochaAndAssertResult(page) {
   await page.waitForFunction(() => window.mochaResults); // eslint-disable-line no-undef
@@ -9,15 +9,5 @@ async function waitForMochaAndAssertResult(page) {
 
 test('Spec handlebars.js', async ({ page, baseURL }) => {
   await page.goto(`${baseURL}/spec/?headless=true`);
-  await waitForMochaAndAssertResult(page);
-});
-
-test('Spec handlebars.js (UMD)', async ({ page, baseURL }) => {
-  await page.goto(`${baseURL}/spec/umd.html?headless=true`);
-  await waitForMochaAndAssertResult(page);
-});
-
-test('Spec handlebars.runtime.js (UMD)', async ({ page, baseURL }) => {
-  await page.goto(`${baseURL}/spec/umd-runtime.html?headless=true`);
   await waitForMochaAndAssertResult(page);
 });
